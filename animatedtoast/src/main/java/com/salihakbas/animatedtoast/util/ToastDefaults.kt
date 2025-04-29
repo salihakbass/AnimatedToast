ccaaa1447bc355a08d8fb0312736751d1e0eb34f
Salih Akbas <salihhakbass@gmail.com>
2025-04-29 15:53:18 +0300

    Add default color definitions for toast message types

---
+++ animatedtoast/src/main/java/com/salihakbas/animatedtoast/util/ToastDefaults.kt
@@ -0,0 +1,9 @@
+package com.salihakbas.animatedtoast.util
+
+import androidx.compose.ui.graphics.Color
+
+object ToastDefaults {
+    var successColor: Color = Color(0xFF4CAF50)
+    var errorColor: Color = Color(0xFFF44336)
+    var infoColor: Color = Color(0xFF2196F3)
+}
